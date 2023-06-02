4ad353808ff933b72720d5777de4f75246be2141
DevMinGeonPark <devmin7985@gmail.com>
2023-06-02 22:13:09 +0900

    fix the not necessary comment about train.py

--- lstm_kmean/train.py
+++ lstm_kmean/train.py
@@ -30,10 +30,6 @@ if __name__ == '__main__':
 	test_batch_size  = 1
 	n_classes   = 10
 
-	# data_cls = natsorted(glob('data/thoughtviz_eeg_data/*'))
-	# cls2idx  = {key.split(os.path.sep)[-1]:idx for idx, key in enumerate(data_cls, start=0)}
-	# idx2cls  = {value:key for key, value in cls2idx.items()}
-
 	with open('data/eeg/image/data.pkl', 'rb') as file:
 		data = pickle.load(file, encoding='latin1')
 		train_X = data['x_train']
@@ -41,16 +37,11 @@ if __name__ == '__main__':
 		test_X = data['x_test']
 		test_Y = data['y_test']
 
-
-	# train_batch = load_complete_data('data/thoughtviz_eeg_data/*/train/*', batch_size=batch_size)
-	# val_batch   = load_complete_data('data/thoughtviz_eeg_data/*/val/*', batch_size=batch_size)
-	# test_batch  = load_complete_data('data/thoughtviz_eeg_data/*/test/*', batch_size=test_batch_size)
 	train_batch = load_complete_data(train_X, train_Y, batch_size=batch_size)
 	val_batch   = load_complete_data(test_X, test_Y, batch_size=batch_size)
 	test_batch  = load_complete_data(test_X, test_Y, batch_size=test_batch_size)
 	X, Y = next(iter(train_batch))
 
-	# print(X.shape, Y.shape)
 	triplenet = TripleNet(n_classes=n_classes)
 	opt     = tf.keras.optimizers.legacy.Adam(learning_rate=3e-4)
 	triplenet_ckpt    = tf.train.Checkpoint(step=tf.Variable(1), model=triplenet, optimizer=opt)
@@ -73,11 +64,6 @@ if __name__ == '__main__':
 	feat_X = np.array(feat_X)
 	feat_Y = np.array(feat_Y)
 	print(feat_X.shape, feat_Y.shape)
-	# colors = list(plt.cm.get_cmap('viridis', 10))
-	# print(colors)
-	# colors  = [np.random.rand(3,) for _ in range(10)]
-	# print(colors)
-	# Y_color = [colors[label] for label in feat_Y]
 
 	tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=700)
 	tsne_results = tsne.fit_transform(feat_X)
@@ -90,14 +76,10 @@ if __name__ == '__main__':
 
 	df.to_csv('experiments/inference/triplet_embed2D.csv')
 	
-	# df.to_csv('experiments/triplenet_embed3D.csv')
-	# df = pd.read_csv('experiments/triplenet_embed2D.csv')
-	
 	df = pd.read_csv('experiments/inference/triplet_embed2D.csv')
 
 	plt.figure(figsize=(16,10))
 	
-	# ax = plt.axes(projection='3d')
 	sns.scatterplot(
 	    x="x1", y="x2",
 	    data=df,
@@ -108,18 +90,12 @@ if __name__ == '__main__':
 	    )
 
 	plt.show()
-	# plt.savefig('experiments/inference/{}_embedding.png'.format(epoch))
 
 	kmeans = KMeans(n_clusters=n_classes,random_state=45)
 	kmeans.fit(feat_X)
 	labels = kmeans.labels_
-	# print(feat_Y, labels)
+
 	correct_labels = sum(feat_Y == labels)
 	print("Result: %d out of %d samples were correctly labeled." % (correct_labels, feat_Y.shape[0]))
 	kmeanacc = correct_labels/float(feat_Y.shape[0])
 	print('Accuracy score: {0:0.2f}'. format(kmeanacc))
-
-	# with open('experiments/triplenet_log.txt', 'a') as file:
-	# 	file.write('E: {}, Train Loss: {}, Test Loss: {}, KM Acc: {}\n'.\
-	# 		format(epoch, train_loss.result(), test_loss.result(), kmeanacc))
-	# break
